0f79827c5ae409412b97b01a04ba8137a95cfd74
yaskalnazar <yaskalnazar@gmail.com>
2019-08-05 18:20:28 +0300

    log for Validate

--- src/main/java/ua/yaskal/controller/util/ValidationUtil.java
+++ src/main/java/ua/yaskal/controller/util/ValidationUtil.java
@@ -1,33 +1,40 @@
 package ua.yaskal.controller.util;
 
+import org.apache.log4j.Logger;
+import ua.yaskal.controller.command.user.NewCreditRequestCommand;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class ValidationUtil{
+public class ValidationUtil {
     private ResourceBundle sqlRequestsBundle = ResourceBundle.getBundle("regex");
+    private final static Logger logger = Logger.getLogger(ValidationUtil.class);
 
 
-    //TODO stream
-    public boolean isContains(HttpServletRequest request, List<String> params){
-        for (String param: params) {
-            if (Objects.isNull(request.getParameter(param)) || request.getParameter(param).isEmpty())
+    public boolean isContains(HttpServletRequest request, List<String> params) {
+        for (String param : params) {
+            if (Objects.isNull(request.getParameter(param)) || request.getParameter(param).isEmpty()) {
+                logger.warn("Null or empty parameter: " + param);
                 return false;
+            }
         }
         return true;
     }
 
 
-    public boolean isRequestValid(HttpServletRequest request, List<String> paramNames){
-        for (String paramName:paramNames){
-            if (!isParamValid(request.getParameter(paramName), paramName))
+    public boolean isRequestValid(HttpServletRequest request, List<String> paramNames) {
+        for (String paramName : paramNames) {
+            if (!isParamValid(request.getParameter(paramName), paramName)) {
+                logger.warn("Incorrect parameter - " + paramName + ": " + request.getParameter(paramName));
                 return false;
+            }
         }
         return true;
     }
 
-    public boolean isParamValid(String param, String paramName){
-        return param.matches(sqlRequestsBundle.getString("regex."+paramName));
+    public boolean isParamValid(String param, String paramName) {
+        return param.matches(sqlRequestsBundle.getString("regex." + paramName));
     }
 }
